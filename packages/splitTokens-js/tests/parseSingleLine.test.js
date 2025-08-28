1e23417e8b32d1085965e14375f35c9912683356
mochizukiaiichiro <mochizukiaiichiro@gmail.com>
2025-08-28 13:40:05 +0900

    テストを修正（{ primitive:true }に対応）

--- packages/splitTokens-js/tests/parseSingleLine.test.js
+++ packages/splitTokens-js/tests/parseSingleLine.test.js
@@ -3,12 +3,12 @@ import parseSingleLine from '../src/parseSingleLine.js';
 describe('parseSingleLine', () => {
   const delimiter = /[ ,]+/;
 
-  test('単一値は値そのまま返す', () => {
-    expect(parseSingleLine('a', delimiter)).toBe('a');
-    expect(parseSingleLine('123', delimiter)).toBe(123);
+  test('単一値', () => {
+    expect(parseSingleLine('a', delimiter)).toEqual(['a']);
+    expect(parseSingleLine('123', delimiter)).toEqual([123]);
   });
 
-  test('複数値は配列で返す', () => {
+  test('複数値', () => {
     expect(parseSingleLine('a b c', delimiter)).toEqual(['a', 'b', 'c']);
     expect(parseSingleLine('1 2 3', delimiter)).toEqual([1, 2, 3]);
   });
--- packages/splitTokens-js/tests/splitTokens.test.js
+++ packages/splitTokens-js/tests/splitTokens.test.js
@@ -1,19 +1,36 @@
 import splitTokens from '../src/splitTokens.js';
 import single from './fixtures/single.json' with { type: 'json' };
+import singleDefault from './fixtures/singleDefault.json' with { type: 'json' };
 import oneLine from './fixtures/oneLine.json' with { type: 'json' };
 import multiLines from './fixtures/multiLines.json' with { type: 'json' };
 import misc from './fixtures/misc.json' with { type: 'json' };
 
 describe('splitTokens', () => {
-  describe('① 単一値（改行無し、区切り文字がないデータ）', () => {
+  describe('【1-2】単一値（改行無し、区切り文字がないデータ、第二引数,{ primitive:true }（戻り値がプリミティブ型））', () => {
     for (const { title, input, output } of single) {
       test(title, () => {
-        expect(splitTokens(input)).toBe(output);
+        expect(splitTokens(input, { primitive:true })).toBe(output);
       });
     }
   });
 
-  describe('② 一行（改行無し、1行の中で区切り文字があるデータ）', () => {
+  describe('【1-2】単一値（改行無し、区切り文字がないデータ、第二引数,default（戻り値が配列型））', () => {
+    for (const { title, input, output } of singleDefault) {
+      test(title, () => {
+        expect(splitTokens(input)).toEqual(output);
+      });
+    }
+  });
+
+  describe('【2-1】一行（改行無し、1行の中で区切り文字があるデータ）、第二引数,{ primitive:true }', () => {
+    for (const { title, input, output } of oneLine) {
+      test(title, () => {
+        expect(splitTokens(input, { primitive:true })).toEqual(output);
+      });
+    };
+  });
+  
+  describe('【2-2】一行（改行無し、1行の中で区切り文字があるデータ）、第二引数,default', () => {
     for (const { title, input, output } of oneLine) {
       test(title, () => {
         expect(splitTokens(input)).toEqual(output);
@@ -21,7 +38,15 @@ describe('splitTokens', () => {
     };
   });
 
-  describe('③ 複数行', () => {
+  describe('【3-1】複数行、第二引数,{ primitive:true }', () => {
+    for (const { title, input, output } of multiLines) {
+      test(title, () => {
+        expect(splitTokens(input, { primitive:true })).toEqual(output);
+      });
+    };
+  });
+  
+  describe('【3-2】複数行、第二引数,default', () => {
     for (const { title, input, output } of multiLines) {
       test(title, () => {
         expect(splitTokens(input)).toEqual(output);
@@ -29,7 +54,15 @@ describe('splitTokens', () => {
     };
   });
 
-  describe('④ 競プロ特有の多様な入力パターン', () => {
+  describe('【4-1】競プロ特有の多様な入力パターン、第二引数,{ primitive:true }', () => {
+    for (const { title, input, output } of misc) {
+      test(title, () => {
+        expect(splitTokens(input, { primitive:true })).toEqual(output);
+      });
+    };
+  });
+  
+  describe('【4-2】競プロ特有の多様な入力パターン、第二引数,default', () => {
     for (const { title, input, output } of misc) {
       test(title, () => {
         expect(splitTokens(input)).toEqual(output);
